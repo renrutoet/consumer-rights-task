527c7a0b1ced9e78cb3377cd5d1b281e6be6a262
Teo Turner <teoturner@gmail.com>
2023-07-19 16:26:56 +0100

    feature: implemented backgrounds for each section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,20 @@ import styles from "./styles/Page.module.scss";
 export default function Home() {
   return (
     <main>
-      <section className={`${styles.section} ${styles["background--blue"]} `}>
-        Hello World
+      <section
+        className={`${styles.section} ${styles["background--blue"]} ${styles["hero"]} `}
+      ></section>
+      <section className={`${styles.section} ${styles["what-we-do"]}`}>
+        <div className={` ${styles["what-we-do__background-left"]}`}>
+          <div className={`${styles["what-we-do__background-text"]}`}>
+            Automation
+          </div>
+        </div>
       </section>
-      <section className={`${styles.section} ${styles["background--red"]} `}>
-        Hello World
-      </section>
-      <section className={`${styles.section} ${styles["background--green"]} `}>
-        Hello World
-      </section>
-      <section className={`${styles.section} ${styles["background--yellow"]} `}>
-        Hello World
-      </section>
+      <section className={`${styles.section} ${styles["our-work"]} `}></section>
+      <section
+        className={`${styles.section} ${styles["get-in-touch"]} `}
+      ></section>
     </main>
   );
 }
