79e7436524d7b707dfce48c21a6b53972204d2e6
Teo Turner <teoturner@gmail.com>
2023-07-19 11:37:31 +0100

    applied simple styling to block out sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import styles from "./page.module.css";
+import styles from "./styles/Page.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div>Hello World</div>
+    <main>
+      <section className={`${styles.section} ${styles["background--blue"]} `}>
+        Hello World
+      </section>
+      <section className={`${styles.section} ${styles["background--red"]} `}>
+        Hello World
+      </section>
+      <section className={`${styles.section} ${styles["background--green"]} `}>
+        Hello World
+      </section>
+      <section className={`${styles.section} ${styles["background--yellow"]} `}>
+        Hello World
+      </section>
     </main>
   );
 }
