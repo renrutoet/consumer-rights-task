5259befa757520b2ec5a448204a0e331a2e5fae7
Teo Turner <teoturner@gmail.com>
2023-07-20 13:54:16 +0100

    feature: implemented hero basic layout and title content and styling

--- src/app/components/Sections/Hero.tsx
+++ src/app/components/Sections/Hero.tsx
@@ -1,7 +1,26 @@
-import styles from '../../styles/Page.module.scss'
+import PageStyles from '../../styles/Page.module.scss'
+import HeroStyles from './Hero.module.scss'
 
 export const Hero = () => {
     return (
-        <section className={`${styles.section} ${styles['hero']} `}></section>
+        <section className={`${PageStyles.section}  ${HeroStyles.hero} `}>
+            <div>Nav Bar</div>
+            <div className={`${HeroStyles['hero-title']}`}>
+                <p className={`${HeroStyles['hero-title__pre-text']}`}>
+                    We are the{' '}
+                </p>
+                <h1 className={`${HeroStyles['hero-title__main-text']}`}>
+                    Digital Litigation Partners
+                </h1>
+            </div>
+            <div className={`${HeroStyles['call-to-action']}`}>
+                <button className={`${HeroStyles['call-to-action__button']}`}>
+                    LEARN MORE
+                </button>
+                <div className={`${HeroStyles['call-to-action__arrow']}`}>
+                    arrow
+                </div>
+            </div>
+        </section>
     )
 }
