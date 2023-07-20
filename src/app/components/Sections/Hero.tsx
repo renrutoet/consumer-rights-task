54429348172911e67a126be4cdc16e03d433d5cc
Teo Turner <teoturner@gmail.com>
2023-07-20 13:09:50 +0100

    chore: abstract Hero to component file

---
+++ src/app/components/Sections/Hero.tsx
@@ -0,0 +1,7 @@
+import styles from '../../styles/Page.module.scss'
+
+export const Hero = () => {
+    return (
+        <section className={`${styles.section} ${styles['hero']} `}></section>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
-import styles from "./styles/Page.module.scss";
-import { SplitBackground } from "./components/SplitBackground/SplitBackground";
+import styles from './styles/Page.module.scss'
+import { SplitBackground } from './components/SplitBackground/SplitBackground'
+import { Hero } from './components/Sections/Hero'
 
 export default function Home() {
-  return (
-    <main>
-      <section
-        className={`${styles.section} ${styles["background--blue"]} ${styles["hero"]} `}
-      ></section>
-      <section className={`${styles.section} ${styles["what-we-do"]}`}>
-        <SplitBackground text="Automation"></SplitBackground>
-      </section>
-      <section className={`${styles.section} ${styles["our-work"]} `}></section>
-      <section
-        className={`${styles.section} ${styles["get-in-touch"]} `}
-      ></section>
-    </main>
-  );
+    return (
+        <main>
+            <Hero />
+            <section className={`${styles.section} ${styles['what-we-do']}`}>
+                <SplitBackground text="Automation"></SplitBackground>
+            </section>
+            <section
+                className={`${styles.section} ${styles['our-work']} `}
+            ></section>
+            <section
+                className={`${styles.section} ${styles['get-in-touch']} `}
+            ></section>
+        </main>
+    )
 }
