ca0f1d080002da0817bbb84f1df403920d5cdfe2
Teo Turner <teoturner@gmail.com>
2023-07-21 16:50:39 +0100

    chore: tidied up files and filled out font class

--- src/app/components/Sections/Contact/Contact.tsx
+++ src/app/components/Sections/Contact/Contact.tsx
@@ -1,8 +1,5 @@
-import { IconWrapper } from '../../Icon/Icon'
 import pageStyles from '../../../styles/Page.module.scss'
-import textStyles from '../../../styles/text.module.scss'
 import contactStyles from './Contact.module.scss'
-import { Row } from '../../Row/Row'
 import { ContactMethods } from './ContactMethods'
 import { ContactForm } from './ContactForm'
 import { ContactFooter } from './ContactFooter'
