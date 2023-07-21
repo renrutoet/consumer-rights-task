551b37294c7d1cdefc23cb5fe0c10e2846b7c3bc
Teo Turner <teoturner@gmail.com>
2023-07-21 16:51:34 +0100

    feat: implemented work section layout and basic content

---
+++ src/app/components/Sections/OurWork/OurWork.tsx
@@ -0,0 +1,106 @@
+import { ReactNode } from 'react'
+import pageStyles from '../../../styles/Page.module.scss'
+import textStyles from '../../../styles/text.module.scss'
+import ourWorkstyles from './OurWork.module.scss'
+
+const mockData = [
+    {
+        title: 'Web design and development',
+        body: 'Donec sed eros augue. Aliquam aliquet velit eget massa luctus, quis consectetur ex interdum. Cras nibh enim, vulputate necturpis ac, tincidunt molestie nibh. Aliquam a cursus felis. Sedvestibulum id quam feugiat efficitur. Etiam rhoncus pretium quamat cursus. Donec ac porttitor neque. In venenatis at lectus nontempus.',
+        buttonText: 'LEARN MORE',
+    },
+    {
+        title: 'Search engine optimization',
+        body: 'Donec sed eros augue. Aliquam aliquet velit eget massa luctus, quis consectetur ex interdum. Cras nibh enim, vulputate necturpis ac, tincidunt molestie nibh. Aliquam a cursus felis. Sedvestibulum id quam feugiat efficitur. Etiam rhoncus pretium quamat cursus. Donec ac porttitor neque. In venenatis at lectus nontempus.',
+        buttonText: 'LEARN MORE',
+    },
+]
+
+interface ContentProps {
+    title: ReactNode
+    body: string
+    buttonText: string
+    alignment?: 'left' | 'right'
+}
+
+const WebSectionTitle = (): JSX.Element => {
+    return (
+        <h4
+            className={`${textStyles['font-primary']} ${ourWorkstyles['content__title']}`}
+        >
+            <span className={`${textStyles['font-primary--italic']}`}>
+                Web{' '}
+            </span>
+            <span className={`${textStyles['font-secondary--italic']}`}>
+                design
+            </span>{' '}
+            <p>and development</p>
+        </h4>
+    )
+}
+const SEOSectionTitle = (): JSX.Element => {
+    return (
+        <h4
+            className={`${textStyles['font-primary']} ${ourWorkstyles['content__title']}`}
+        >
+            <span className={`${textStyles['font-primary--italic']}`}>
+                Search{' '}
+            </span>
+            <span className={`${textStyles['font-secondary--italic']}`}>
+                engine
+            </span>{' '}
+            <p>optimization</p>
+        </h4>
+    )
+}
+
+const Content = ({
+    title,
+    body,
+    buttonText,
+    alignment,
+}: ContentProps): JSX.Element => {
+    const containerClass = alignment
+        ? `${ourWorkstyles[`content--${alignment}`]} `
+        : `${ourWorkstyles['content']}`
+    return (
+        <div className={containerClass}>
+            {title}
+            <div
+                className={`${textStyles['font-primary']} ${ourWorkstyles['content__body']}`}
+            >
+                {body}
+            </div>
+            <button className={`${pageStyles['button']}`}>{buttonText}</button>
+        </div>
+    )
+}
+
+export const OurWork = () => {
+    return (
+        <section className={`${ourWorkstyles['our-work']} `}>
+            <div className={`${ourWorkstyles['our-work__grid']} `}>
+                <div></div>
+                <div className={`${ourWorkstyles['content__dash']} `}></div>
+                <div></div>
+                <div>TECH ICONS</div>
+                <div className={`${ourWorkstyles['content__dash']} `}></div>
+                <Content
+                    title={WebSectionTitle()}
+                    body={mockData[0].body}
+                    buttonText={mockData[0].buttonText}
+                    alignment="left"
+                />
+
+                <Content
+                    title={SEOSectionTitle()}
+                    body={mockData[1].body}
+                    buttonText={mockData[1].buttonText}
+                    alignment="right"
+                />
+                <div className={`${ourWorkstyles['content__dash']} `}></div>
+                <div>PHONE IMAGE</div>
+            </div>
+        </section>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from './styles/Page.module.scss'
 import { SplitBackground } from './components/SplitBackground/SplitBackground'
 import { Hero } from './components/Sections/Hero/Hero'
 import { Contact } from './components/Sections/Contact/Contact'
+import { OurWork } from './components/Sections/OurWork/OurWork'
 
 export default function Home() {
     return (
@@ -12,9 +13,7 @@ export default function Home() {
             <section className={`${styles.section} ${styles['what-we-do']}`}>
                 <SplitBackground text="Automation"></SplitBackground>
             </section>
-            <section
-                className={`${styles.section} ${styles['our-work']} `}
-            ></section>
+            <OurWork />
             <Contact />
         </main>
     )
