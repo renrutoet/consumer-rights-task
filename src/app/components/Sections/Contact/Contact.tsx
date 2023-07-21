92532794fa899d2f686929efc0aa223b5a909806
Teo Turner <teoturner@gmail.com>
2023-07-21 20:20:40 +0100

    feat: optimise some background images

--- src/app/components/Sections/Contact/Contact.tsx
+++ src/app/components/Sections/Contact/Contact.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import dlpFooter from '../../../../../public/dlp-footer-background.jpg'
 import pageStyles from '../../../styles/Page.module.scss'
 import contactStyles from './Contact.module.scss'
 import { ContactMethods } from './ContactMethods'
@@ -9,6 +11,16 @@ export const Contact = () => {
         <section
             className={`${pageStyles.section} ${contactStyles['get-in-touch']} `}
         >
+            <Image
+                alt="faint image of office"
+                src={dlpFooter}
+                placeholder="blur"
+                fill
+                style={{
+                    zIndex: -1,
+                    objectFit: 'cover',
+                }}
+            />
             <ContactMethods />
             <ContactForm />
             <ContactFooter />
--- src/app/components/Sections/Hero/Hero.tsx
+++ src/app/components/Sections/Hero/Hero.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import dlpBanner from '../../../../../public/dlp-banner.jpg'
 import PageStyles from '../../../styles/Page.module.scss'
 import HeroStyles from './Hero.module.scss'
 import { Navbar } from '../../Navbar/Navbar'
@@ -10,6 +12,16 @@ export const Hero = () => {
             className={`${PageStyles.section}  ${HeroStyles.hero} `}
             id="hero"
         >
+            <Image
+                alt="3d graph graphic"
+                src={dlpBanner}
+                placeholder="blur"
+                fill
+                style={{
+                    zIndex: -1,
+                    objectFit: 'cover',
+                }}
+            />
             <Navbar logoUrl="logo" links={heroData.navbar.links} />
             <div className={`${HeroStyles['hero-title']}`}>
                 <p className={`${HeroStyles['hero-title__pre-text']}`}>
