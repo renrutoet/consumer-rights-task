670755b55d713ca133b45f201db68d2fe1869379
Teo Turner <teoturner@gmail.com>
2023-07-22 19:42:05 +0100

    feat: added border details to contact section

--- src/app/components/Sections/Contact/ContactMethods.tsx
+++ src/app/components/Sections/Contact/ContactMethods.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { IconWrapper } from '../../Icon/Icon'
 import { Row } from '../../Row/Row'
 import contactStyles from './Contact.module.scss'
+import pageStyles from '../../../styles/Page.module.scss'
 import phoneIcon from '../../../../../public/icons/phone-incoming-call.svg'
 import houseIcon from '../../../../../public/icons/house-chimney-2.svg'
 import emailIcon from '../../../../../public/icons/send-email-1.svg'
@@ -41,22 +42,36 @@ const mockContactMethodData = [
 
 export const ContactMethods = (): JSX.Element => {
     return (
-        <div className={`${contactStyles['contact-methods']} `}>
-            <Row height="full">
-                {mockContactMethodData
-                    ? mockContactMethodData.map((iconData, index) => {
-                          return (
-                              <IconWrapper
-                                  key={`contact-methods-${index}`}
-                                  text={iconData.text}
-                                  circle
-                              >
-                                  {iconData.content}
-                              </IconWrapper>
-                          )
-                      })
-                    : null}
-            </Row>
-        </div>
+        <>
+            <div className={`${contactStyles['contact-methods']} `}>
+                <div
+                    className={`${contactStyles['contact-methods__border-details--top']}`}
+                ></div>
+
+                <Row height="full">
+                    {mockContactMethodData
+                        ? mockContactMethodData.map((iconData, index) => {
+                              return (
+                                  <IconWrapper
+                                      key={`contact-methods-${index}`}
+                                      text={iconData.text}
+                                      circle
+                                  >
+                                      {iconData.content}
+                                  </IconWrapper>
+                              )
+                          })
+                        : null}
+                </Row>
+                <div
+                    className={`${contactStyles['contact-methods__border-details--bottom']}`}
+                ></div>
+            </div>
+            <div>
+                <div
+                    className={`${pageStyles['centered-dash--bottom']} ${pageStyles['centered-dash--relative']}`}
+                ></div>
+            </div>
+        </>
     )
 }
