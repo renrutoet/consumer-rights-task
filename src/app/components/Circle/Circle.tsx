529a751521cc9846ea2592e3871fdbc98ae01a66
Teo Turner <teoturner@gmail.com>
2023-07-22 20:12:25 +0100

    feat: create Circle graphic component

---
+++ src/app/components/Circle/Circle.tsx
@@ -0,0 +1,19 @@
+import styles from './Circle.module.scss'
+
+interface CircleProps {
+    greyScale?: boolean
+}
+
+export const Circle = ({ greyScale = false }: CircleProps): JSX.Element => {
+    const classExtension = greyScale ? '--greyscale' : ''
+
+    return (
+        <div className={`${styles[`outer-circle${classExtension}`]}`}>
+            <div className={`${styles[`inner-circle${classExtension}`]}`}>
+                <div
+                    className={`${styles[`center-dot${classExtension}`]}`}
+                ></div>
+            </div>
+        </div>
+    )
+}
