d63eff9d3cd4ca7a8225bffc661fbd62660fbdde
Teo Turner <teoturner@gmail.com>
2023-07-23 14:21:30 +0100

    feat: form accessibility improvements

--- src/app/components/Sections/Contact/ContactForm.tsx
+++ src/app/components/Sections/Contact/ContactForm.tsx
@@ -4,27 +4,50 @@ import pageStyles from '../../../styles/Page.module.scss'
 export const ContactForm = () => {
     return (
         <div className={`${contactStyles['contact']} `}>
-            <h2 className={`${contactStyles['contact__title']} `}>
-                <em>Enquire online</em>
-            </h2>
-            <div className={`${contactStyles['contact__form']} `}>
-                <div
-                    className={`${contactStyles['contact__form__left-column']}`}
-                >
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email address" />
-                    <input type="number" placeholder="Phone number" />
-                    <button
-                        className={`${pageStyles['button']} ${pageStyles['button--small']}`}
-                    >
-                        Send Message*
-                    </button>
-                </div>
-                <textarea
-                    placeholder="Message"
-                    className={`${contactStyles['contact__form__message']} `}
-                />
-            </div>
+            <form>
+                <fieldset>
+                    <legend>
+                        <h2 className={`${contactStyles['contact__title']} `}>
+                            <em>Enquire online</em>
+                        </h2>
+                    </legend>
+
+                    <div className={`${contactStyles['contact__form']} `}>
+                        <div
+                            className={`${contactStyles['contact__form__left-column']}`}
+                        >
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                aria-label="name field"
+                            />
+                            <input
+                                type="email"
+                                placeholder="Email address"
+                                aria-label="email field"
+                            />
+                            <input
+                                type="number"
+                                placeholder="Phone number"
+                                aria-label="phone number field"
+                            />
+                            <button
+                                type="submit"
+                                className={`${pageStyles['button']} ${pageStyles['button--small']}`}
+                                aria-label="submit message button"
+                            >
+                                Send Message*
+                            </button>
+                        </div>
+                        <textarea
+                            placeholder="Message"
+                            className={`${contactStyles['contact__form__message']} `}
+                            aria-label="message field"
+                        />
+                    </div>
+                </fieldset>
+            </form>
+
             <p className={`${contactStyles['contact__legal']}`}>
                 *By pressing send you consent to Digital Litigation Partners
                 collecting your submitted information in order to respond to
